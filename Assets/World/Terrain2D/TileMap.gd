2afff00eae81d6ac89538882301ee60d0d491a3c
theludovyc <theludovyc@gmail.com>
2024-02-07 18:17:21 +0100

    [wip] start read a json to load map

--- Assets/World/Terrain2D/TileMap.gd
+++ Assets/World/Terrain2D/TileMap.gd
@@ -2,48 +2,33 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var file = FileAccess.open("res://Assets/World/Terrain2D/mp_dev.csv", FileAccess.READ)
+	var file = FileAccess.open("res://Assets/World/Terrain2D/mp_dev.json", FileAccess.READ)
 	
-	var deep_cells:PackedVector2Array
-	var shallow_cells:PackedVector2Array
-	var sand_cells:PackedVector2Array
-	var ground_cells:PackedVector2Array
+	var json = JSON.new()
 	
-	while(!file.eof_reached()):
-		var line = file.get_csv_line()
-		
-		if line.is_empty() or line.size() < 3:
-			continue
+	if json.parse(file.get_as_text()) != OK:
+		return
+	
+	for island in json.data["islands"]:
+		var deep_cells:PackedVector2Array
+		var shallow_cells:PackedVector2Array
+		var sand_cells:PackedVector2Array
+		var ground_cells:PackedVector2Array
 			
-		var atlas_pos:Vector2i
-		
-		var tile_vec = Vector2i(int(line[0]), int(line[1]))
-		
-		match int(line[2]):
-			2:
-				atlas_pos = Vector2i(1, 2)
-				deep_cells.push_back(tile_vec)
+		var set_cells = func(array_in, atlas_pos, array_out):
+			for i in range(0, array_in.size(), 2):
+				var tile_vec = Vector2i(array_in[i], array_in[i + 1])
+				array_out.push_back(tile_vec)
+				set_cell(0, tile_vec, 1, atlas_pos)
 				
-			3:
-				atlas_pos = Vector2i(7, 6)
-				ground_cells.push_back(tile_vec)
-				
-			4:
-				atlas_pos = Vector2i(7, 2)
-				sand_cells.push_back(tile_vec)
-				
-			_:
-				atlas_pos = Vector2i(4, 2)
-				shallow_cells.push_back(tile_vec)
+		set_cells.call(island["deep_tiles"], Vector2i(1, 2), deep_cells)
+		set_cells.call(island["shallow_tiles"], Vector2i(4, 2), shallow_cells)
+		set_cells.call(island["sand_tiles"], Vector2i(7, 2), sand_cells)
+		set_cells.call(island["ground_tiles"], Vector2i(7, 6), ground_cells)
 	
-		set_cell(0, tile_vec, 1, atlas_pos)
-	
-	set_cells_terrain_connect(0, ground_cells, 0, 3)
-	
-	set_cells_terrain_connect(0, sand_cells, 0, 2)
-	
-	set_cells_terrain_connect(0, shallow_cells, 0, 1)
-	
+		set_cells_terrain_connect(0, ground_cells, 0, 3)
+		set_cells_terrain_connect(0, sand_cells, 0, 2)
+		set_cells_terrain_connect(0, shallow_cells, 0, 1)
 	pass # Replace with function body.
 
 
