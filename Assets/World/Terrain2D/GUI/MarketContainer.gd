80558c577ae23b7c367dfaa6767e4bad3d919718
theludovyc <user.email>
2024-07-15 16:24:11 +0200

    Finally simplify the order
    Remove the ResourceButton in ResourceOrder
    Replace it with a TextureRect
    Finaly, do not replace the resource type when clic on the button

--- Assets/World/Terrain2D/GUI/MarketContainer.gd
+++ Assets/World/Terrain2D/GUI/MarketContainer.gd
@@ -6,33 +6,15 @@ var resource_order_scene = preload("res://Assets/World/Terrain2D/GUI/ResourceOrd
 
 @onready var order_container = %OrderContainer
 
-var is_new_order := false
-
-var current_resource_order:Node = null
-
 func _ready():
 	resource_popup.result.connect(_on_ResourcePopup_result)
 
 func _on_ResourcePopup_result(resource_type:Resources.Types):
-	if is_new_order:
-		var resource_order = resource_order_scene.instantiate()
-		
-		order_container.add_child(resource_order)
-		
-		resource_order._resource_type = resource_type
-		resource_order._resource_button.pressed.connect(
-			_on_OrderResourceButton_pressed.bind(resource_order))
-		
-		is_new_order = false
-	elif current_resource_order != null:
-		current_resource_order._resource_type = resource_type
-
-func _on_OrderResourceButton_pressed(node:Control):
-	current_resource_order = node
+	var resource_order = resource_order_scene.instantiate()
 	
-	resource_popup.popup_centered()
+	order_container.add_child(resource_order)
+	
+	resource_order._resource_type = resource_type
 
 func _on_NewOrderButton_pressed():
-	is_new_order = true
-	
 	resource_popup.popup_centered()
--- Assets/World/Terrain2D/GUI/ResourceOrder.gd
+++ Assets/World/Terrain2D/GUI/ResourceOrder.gd
@@ -1,10 +1,11 @@
 extends HBoxContainer
 
-@onready var _resource_button = $VBoxContainer/TextureButton
+@onready var _resource_texture = $VBoxContainer/TextureRect
 
 var _resource_type:Resources.Types:
 	set(value):
 		_resource_type = value
 		
-		if is_instance_valid(_resource_button):
-			_resource_button.set_resource_icon(_resource_type)
+		if is_instance_valid(_resource_texture):
+			if Resources.Icons.has(value):
+				_resource_texture.texture = Resources.Icons[value]
